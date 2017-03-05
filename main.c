a1568fcfe55d9faffffb79b2f50adc8786968546
Kristina <kristin161098@mail.ru>
2017-03-05 20:54:54 +0700

    first

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+	int srok;
+	float summa, summao;
+	printf("Input srok vklada:");
+	scanf("%f", &srok);
+	printf("Input summu vklada:");
+	scanf("%f", &summa);
+	summao=summa;
+	return 0;
+}
