e31cf8467fa146b1bb3f6468ac1bb875112cd1df
Kristina <kristin161098@mail.ru>
2017-03-05 21:04:29 +0700

    Tretiy

--- main.c
+++ main.c
@@ -11,5 +11,20 @@ int main()
 	summao=summa;
 	if ((srok<0) || (srok>365)) printf("\nIncorrect data");
 	if (summa<10000) printf("\nIncorrect data");
+	if (summa<100000)
+	{
+		if (srok<31) summao-=0.1*summa;
+		if ((srok>30) && (srok<121)) summao+=0.02*summa;
+		if ((srok>120) && (srok<241)) summao+=0.06*summa;
+		if ((srok>240) && (srok<366)) summao+=0.12*summa;
+	}
+	if (summa>=100000)
+	{
+		if (srok<31) summao-=0.1*summa;
+		if ((srok>30) && (srok<121)) summao+=0.03*summa;
+		if ((srok>120) && (srok<241)) summao+=0.08*summa;
+		if ((srok>240) && (srok<366)) summao+=0.15*summa;
+	}
+	printf("\nSumma okonchaniya vklada: %f", summao);
 	return 0;
 }
